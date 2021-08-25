b0dcb26b577d1403147d40bd9da2a868d7e50d01
mezyn <84513662+mezyn@users.noreply.github.com>
2021-08-25 15:10:18 +0200

    Delete Uppgift_1.py

--- Uppgift_1.py
+++
@@ -1,9 +0,0 @@
-print("Välkomna till denna pensionssimulator")
-print("Vad heter du i förnamn?")
-förnamn = input()
-print("Hur gammal är du?")
-ålder = int(input())
-år_till_pension =  65 - ålder
-print("Hej " + förnamn + ". Du går i pension om " + str(år_till_pension) + " år.")
-print("Tryck på valfri tangent för att förtsätta . . .")
-input()
